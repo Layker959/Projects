4fcedb374f5586be8afbf2c39cbacd031e700c36
Luka <ivanstepanov070@gmail.com>
2025-10-23 21:18:20 +0300

    C++ snake_case assignment

---
+++ C++ Задание snake_case/C++ Задание snake_case/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//snake_case
+int main()
+{
+	int first_number = 11;
+	int second_number = -100;
+	int mul = first_number * second_number;
+	cout << "Exit:" << mul << endl;
+	return 0;
+}
